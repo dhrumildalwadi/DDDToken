9fb120b34ae35257d595094d8c3d5b4c67221475
dhrumildalwadi <dhrumildalwadi@gmail.com>
2021-08-18 22:59:45 +0530

    complete

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var DDDToken = artifacts.require("./DDDToken.sol");
+var DDDTokenSale = artifacts.require("./DDDTokenSale.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(DDDToken,1000000);
+    deployer.deploy(DDDToken,1000000).then(function(){
+
+        var tokenPrice = 1000000000000000;
+        return deployer.deploy(DDDTokenSale, DDDToken.address, tokenPrice);
+    });
 };
---
+++ src/js/app.js
@@ -0,0 +1,143 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+  loading: false,
+  tokenPrice: 1000000000000000,
+  tokensSold: 0,
+  tokensAvailable: 750000,
+
+  initMetamask: function() {
+    async function enableUser(){
+      const accounts = await ethereum.enable();
+      const account = accounts[0]
+      App.account = account
+    }
+    enableUser()
+  },
+  
+  init: function() {
+    console.log("App initialized...")
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+
+  initContracts: function() {
+    $.getJSON("DDDTokenSale.json", function(DDDTokenSale) {
+      App.contracts.DDDTokenSale = TruffleContract(DDDTokenSale);
+      App.contracts.DDDTokenSale.setProvider(App.web3Provider);
+      App.contracts.DDDTokenSale.deployed().then(function(DDDTokenSale) {
+        console.log("Dapp Token Sale Address:", DDDTokenSale.address);
+      });
+    }).done(function() {
+      $.getJSON("DDDToken.json", function(DDDToken) {
+        App.contracts.DDDToken = TruffleContract(DDDToken);
+        App.contracts.DDDToken.setProvider(App.web3Provider);
+        App.contracts.DDDToken.deployed().then(function(DDDToken) {
+          console.log("Dapp Token Address:", DDDToken.address);
+        });
+
+        App.listenForEvents();
+        return App.render();
+      });
+    })
+  },
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.DDDTokenSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      })
+    })
+  },
+
+  render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#accountAddress').html("Your Account: " + account);
+      }
+    })
+
+    // Load token sale contract
+    App.contracts.DDDTokenSale.deployed().then(function(instance) {
+      DDDTokenSaleInstance = instance;
+      return DDDTokenSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return DDDTokenSaleInstance.tokensSold();
+    }).then(function(tokensSold) {
+      App.tokensSold = tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+
+      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      $('#progress').css('width', progressPercent + '%');
+
+      // Load token contract
+      App.contracts.DDDToken.deployed().then(function(instance) {
+        DDDTokenInstance = instance;
+        return DDDTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.pnp-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
+  },
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.DDDTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...")
+      $('form').trigger('reset') // reset number of tokens in form
+      // Wait for Sell event
+    });
+  }
+}
+
+$(function() {
+  $(window).load(function() {
+    App.initMetamask();
+    App.init();
+  })
+});
